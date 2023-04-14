72f2cb269c54669c63c4142f1bb3a69b201af9ea
knc6 <writetokamal.1989@gmail.com>
2023-04-14 00:38:34 -0400

    Update version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jarvis_leaderboard",  # Replace with your own username
-    version="2023.04.12",
+    version="2023.04.13",
     author="Kamal Choudhary",
     author_email="kamal.choudhary@nist.gov",
     description="jarvis_leaderboard",
