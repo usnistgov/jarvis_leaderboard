b548f2e1a83abb82f100b109a634954ebb9242bb
knc6 <writetokamal.1989@gmail.com>
2023-04-24 11:26:52 -0400

    Add path in populate.

--- jarvis_leaderboard/jarvis_serve.py
+++ jarvis_leaderboard/jarvis_serve.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 import os
-
+root_dir = os.path.dirname(os.path.abspath(__file__))
 cwd = os.getcwd()
-# os.chdir("jarvis_leaderboard")
+os.chdir(root_dir)
 cmd = "mkdocs serve"
 os.system(cmd)
 os.chdir(cwd)
