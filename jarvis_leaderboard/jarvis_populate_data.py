7dafc9ba65d2f6a64a551431f051dbdc72e5e04a
knc6 <writetokamal.1989@gmail.com>
2023-04-25 13:58:03 -0400

    Modify populate script.

--- jarvis_leaderboard/jarvis_populate_data.py
+++ jarvis_leaderboard/jarvis_populate_data.py
@@ -8,7 +8,7 @@ import time
 import json
 import zipfile
 from jarvis.core.atoms import Atoms
-from jarvis.db.jsonutils import loadjson
+from jarvis.db.jsonutils import loadjson, dumpjson
 import argparse
 from jarvis.db.figshare import data
 import jarvis_leaderboard
@@ -19,7 +19,8 @@ root_dir = str(
 parser = argparse.ArgumentParser(description="JARVIS-Leaderboard")
 parser.add_argument(
     "--benchmark_file",
-    default="SinglePropertyPrediction-test-exfoliation_energy-dft_3d-AI-mae",
+    default="AI-SinglePropertyPrediction-exfoliation_energy-dft_3d-test-mae",
+    # default="SinglePropertyPrediction-test-exfoliation_energy-dft_3d-AI-mae",
     help="Benchmarks available in jarvis_leaderboard/benchmarks/*/*.zip",
 )
 parser.add_argument(
@@ -39,22 +40,24 @@ def get_val(df=None, id_tag="jid", prop="", jv_id="JVASP-14441"):
     return df[df[id_tag] == jv_id][prop].values[0]
 
 
-if __name__ == "__main__":
-    args = parser.parse_args(sys.argv[1:])
-    benchmark_file = args.benchmark_file
-    dataset = benchmark_file.split("-")[3]
-    output_path = args.output_path
-    prop = benchmark_file.split("-")[2]
-    method = benchmark_file.split("-")[4]
-    task = benchmark_file.split("-")[0]
-    id_tag = args.id_tag
-    print("benchmark_file", benchmark_file)
-    print("dataset", dataset)
-    print("output_path", output_path)
-    print("property", prop)
-    print("method", method)
-    print("task", task)
-    print("id_tag", id_tag)
+def get_dataset(
+    benchmark_file="",
+    dataset="",
+    output_path="",
+    prop="",
+    method="",
+    task="",
+    id_tag="",
+    filename="dataset_info.json",
+):
+    b_info = {}
+    b_info["benchmark_file"] = benchmark_file
+    b_info["dataset"] = dataset
+    b_info["output_path"] = output_path
+    b_info["prop"] = prop
+    b_info["methods"] = method
+    b_info["id_tag"] = id_tag
+
     temp = dataset + "_" + prop + ".json.zip"
     temp2 = dataset + "_" + prop + ".json"
     fname = os.path.join(root_dir, "dataset", method, task, temp)
@@ -100,5 +103,41 @@ if __name__ == "__main__":
         print("number of training samples", len(train))
         print("number of validation samples", len(val))
         print("number of test samples", len(test))
+        b_info["n_train"] = len(train)
+        b_info["n_val"] = len(val)
+        b_info["n_test"] = len(test)
+        filename = os.path.join(output_path, filename)
+        dumpjson(data=b_info, filename=filename)
+        return info
         # os.chdir(cwd)
+
+
 # jarvis_leaderboard/dataset/AI/PP/dft_3d_exfoliation_energy.json.zip
+if __name__ == "__main__":
+    args = parser.parse_args(sys.argv[1:])
+    benchmark_file = args.benchmark_file
+    method = benchmark_file.split("-")[0]
+    task = benchmark_file.split("-")[1]
+    prop = benchmark_file.split("-")[2]
+    dataset = benchmark_file.split("-")[3]
+    output_path = args.output_path
+    # method = benchmark_file.split("-")[4]
+    # task = benchmark_file.split("-")[0]
+    id_tag = args.id_tag
+    print("benchmark_file", benchmark_file)
+    print("dataset", dataset)
+    print("output_path", output_path)
+    print("property", prop)
+    print("method", method)
+    print("task", task)
+    print("id_tag", id_tag)
+
+    info = get_dataset(
+        benchmark_file=benchmark_file,
+        dataset=dataset,
+        output_path=output_path,
+        prop=prop,
+        method=method,
+        task=task,
+        id_tag=id_tag,
+    )
