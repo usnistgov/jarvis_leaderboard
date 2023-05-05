4ca5a450973e95e0a3931e5ecb3769676cb6672a
knc6 <writetokamal.1989@gmail.com>
2023-05-05 14:56:26 -0400

    Add multiple cgcnn and dimenetPP contributions, update mutimae metric.

---
+++ jarvis_leaderboard/contributions/kgcnn_dimenetPP/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=162:00:00
+#SBATCH --mem=180G
+#SBATCH --gres=gpu:1
+#SBATCH --partition=singlegpu
+#SBATCH --error=job.err
+#SBATCH --output=job.out
+. ~/.bashrc
+export TMPDIR=/scratch/$SLURM_JOB_ID
+
+cd /wrk/knc6/version_tests/tests/jarvis_leaderboard/jarvis_leaderboard/benchmarks/kgcnn_dimenetPP
+conda activate /wrk/knc6/Software/kgcnn38
+python run.py
