3a4b7e08932677d5cc5c4c23a3a3cb366fad5384
Kangming Li <27884378+mathsphy@users.noreply.github.com>
2023-05-22 18:57:46 -0400

    Update scripts and results

--- jarvis_leaderboard/contributions/matminer_xgboost/run.py
+++ jarvis_leaderboard/contributions/matminer_xgboost/run.py
@@ -9,17 +9,13 @@ import csv
 import numpy as np
 import math
 from jarvis.ai.pkgs.utils import regr_scores
-from jarvis.db.figshare import data
+from jarvis.db.figshare import data, get_request_data
 from jarvis.core.atoms import Atoms
 import zipfile
 import json
 import time
 
 tqdm.pandas()
-
-
-task = 'SinglePropertyClass' # 'SinglePropertyClass'
-
 
 #%%
 '''
@@ -31,36 +27,35 @@ from sklearn.impute import SimpleImputer
 from sklearn.pipeline import Pipeline
 import xgboost as xgb
 
+def get_model(task):
+    if task == 'SinglePropertyPrediction':
+        model =  xgb.XGBRegressor
+        eval_metric=['rmse','mae']
+    elif task == 'SinglePropertyClass':
+        model =  xgb.XGBClassifier
+        eval_metric=['auc']
 
+    n_estimators = 10000
+    num_parallel_tree = 8
+    learning_rate = 0.1  
+    tree_method = 'gpu_hist'   # gpu_hist or hist
+    reg = Pipeline([
+                ('imputer', SimpleImputer()), 
+                ('scaler', StandardScaler()),
+                ('model', model(
+                                # n_jobs=-1, random_state=0,
+                                n_estimators=n_estimators, learning_rate=learning_rate,
+                                reg_lambda=0.01,reg_alpha=0.01,
+                                #subsample=0.85,
+                                colsample_bytree=0.3,colsample_bylevel=0.5,
+    #                            sampling_method='gradient_based',
+                                num_parallel_tree=num_parallel_tree,
+                                tree_method=tree_method,
+                                # eval_metric=eval_metric,
+                                ))
+            ])
+    return reg
 
-#%%
-'''
-Model
-'''
-if task == 'SinglePropertyPrediction':
-    model =  xgb.XGBRegressor
-elif task == 'SinglePropertyClass':
-    model =  xgb.XGBClassifier
-
-
-n_estimators = 10000
-num_parallel_tree = 8
-learning_rate = 0.1  
-tree_method = 'gpu_hist'   # gpu_hist or hist
-reg = Pipeline([
-            ('imputer', SimpleImputer()), 
-            ('scaler', StandardScaler()),
-            ('model', model(
-                            # n_jobs=-1, random_state=0,
-                            n_estimators=n_estimators, learning_rate=learning_rate,
-                            reg_lambda=0.01,reg_alpha=0.01,
-                            subsample=0.85,colsample_bytree=0.3,colsample_bylevel=0.5,
-                            num_parallel_tree=num_parallel_tree,
-                            tree_method=tree_method,
-                            ))
-        ])
-
-#%%
 
 def to_unitcell(structure):
     '''
@@ -78,87 +73,88 @@ def to_unitcell(structure):
     [site.to_unit_cell(in_place=True) for site in structure.sites]
     return structure
 
-# https://github.com/mathsphy/paper-ml-robustness-material-property/blob/main/myfunc.py
-def StructureFeaturizer(
-    df_in, col_id="structure", ignore_errors=True, chunksize=35, index_ids=None
-):
+
+def Featurizer(
+        df,
+        col_id='structure',
+        ignore_errors=True,
+        chunksize=20
+        ):
     """
-    Featurize a dataframe using Matminter Structure featurizer
+    Featurize a dataframe using Matminter featurizers
 
     Parameters
     ----------
-    df : Pandas.DataFrame
+    df : Pandas.DataFrame 
         DataFrame with a column named "structure"
 
     Returns
     -------
-    A DataFrame containing 273 features (columns)
+    A DataFrame containing labels as the first columns and features as the rest 
 
     """
     # For featurization
     from matminer.featurizers.base import MultipleFeaturizer
-    from matminer.featurizers.composition import (
-        ElementProperty,
-        Stoichiometry,
-        ValenceOrbital,
-        IonProperty,
-    )
-    from matminer.featurizers.structure import (
-        SiteStatsFingerprint,
-        StructuralHeterogeneity,
-        ChemicalOrdering,
-        StructureComposition,
-        MaximumPackingEfficiency,
-    )
-    
-    # if not provided, use all the ids in the dataframe
-    if index_ids is None:
-        index_ids = df_in.index
-
-
-    if isinstance(df_in, pd.Series):
-        df = df_in.loc[index_ids].copy().to_frame()
+    from matminer.featurizers.conversions import StrToComposition
+    from matminer.featurizers.composition import (ElementProperty, 
+                                                  Stoichiometry, 
+                                                  ValenceOrbital, 
+                                                  IonProperty)
+    from matminer.featurizers.structure import (SiteStatsFingerprint, 
+                                                StructuralHeterogeneity,
+                                                ChemicalOrdering, 
+                                                StructureComposition, 
+                                                MaximumPackingEfficiency)   
+    # Make sure df is a DataFrame
+    if isinstance(df, pd.Series):
+        df = df.to_frame()   
+    # Use composition featurizers if inputs are compositions, otherwise use
+    # both composition and structure featurizers
+    if col_id != 'structure':
+        # convert string to composition 
+        a = StrToComposition()
+        a._overwrite_data = True
+        df[col_id] = a.featurize_dataframe(df,col_id,pbar=False)['composition']
+        # no structural features
+        struc_feat = []
+        # 145 compositional features
+        compo_feat = [
+            Stoichiometry(),
+            ElementProperty.from_preset("magpie"),
+            ValenceOrbital(props=['frac']),
+            IonProperty(fast=True)
+            ]
     else:
-        df = df_in.loc[index_ids].copy()
-    
-
-    # can we apply this to QM9?
-    df[col_id] = df[col_id].apply(to_unitcell)
-
-    # 128 structural feature
-    struc_feat = [
-        SiteStatsFingerprint.from_preset("CoordinationNumber_ward-prb-2017"),
-        SiteStatsFingerprint.from_preset(
-            "LocalPropertyDifference_ward-prb-2017"
-        ),
-        StructuralHeterogeneity(),
-        MaximumPackingEfficiency(),
-        ChemicalOrdering(),
-    ]
-    # 145 compositional features
-    compo_feat = [
-        StructureComposition(Stoichiometry()),
-        StructureComposition(ElementProperty.from_preset("magpie")),
-        StructureComposition(ValenceOrbital(props=["frac"])),
-        StructureComposition(IonProperty(fast=True)),
-    ]
-    featurizer = MultipleFeaturizer(struc_feat + compo_feat)
-
+        # Ensure sites are within unit cells
+        df[col_id] = df[col_id].apply(to_unitcell)
+        # 128 structural feature
+        struc_feat = [
+            SiteStatsFingerprint.from_preset("CoordinationNumber_ward-prb-2017"), 
+            SiteStatsFingerprint.from_preset("LocalPropertyDifference_ward-prb-2017"),
+            StructuralHeterogeneity(),
+            MaximumPackingEfficiency(),
+            ChemicalOrdering()
+            ]       
+        # 145 compositional features
+        compo_feat = [
+            StructureComposition(Stoichiometry()),
+            StructureComposition(ElementProperty.from_preset("magpie")),
+            StructureComposition(ValenceOrbital(props=['frac'])),
+            StructureComposition(IonProperty(fast=True))
+            ]
+    # Define the featurizer
+    featurizer = MultipleFeaturizer(struc_feat+compo_feat)    
     # Set the chunksize used for Pool.map parallelisation
     featurizer.set_chunksize(chunksize=chunksize)
-    featurizer.fit(df[col_id])
-    X = featurizer.featurize_dataframe(
-        df=df, col_id=col_id, ignore_errors=ignore_errors
-    )
-    # check failed entries
-    print("Featurization completed.")
-    failed = np.any(pd.isnull(X.iloc[:, df.shape[1] :]), axis=1)
+    X = featurizer.featurize_dataframe(df,col_id,ignore_errors=ignore_errors)  
+    # check failed entries    
+    failed = np.any(pd.isnull(X.iloc[:,df.shape[1]:]), axis=1)
     if np.sum(failed) > 0:
-        print(f"Number failed: {np.sum(failed)}/{len(failed)}")
+        print(f'Number failed: {np.sum(failed)}/{len(failed)}')
+    print('Featurization completed.')
     return X, failed
 
 
-#%%    
 # get the available properties for the database db
 def get_props(db):
     dir = f"../../benchmarks/AI/{task}"
@@ -169,54 +165,72 @@ def get_props(db):
     return files 
 
 #%%
-for db in ['dft_3d' ]: #'hmof','qm9','megnet','qe_tb', 'dft_3d', 'ssub',
+
+task = 'SinglePropertyPrediction' # 'SinglePropertyClass','SinglePropertyPrediction',
+reg = get_model(task)
+
+special_dbs = ['ssub','supercon_chem','mag2d_chem']
+
+#for db in special_dbs: # 'hmof','megnet','qe_tb', 'dft_3d', 
+for db in ['qmof',]:
 
     # Get the whole dataset and featurize for once and for all properties 
         
-    if db == 'ssub':
-        dat = pd.read_json('ssub.json')
+    if db in special_dbs:
+        if db == 'ssub':
+            dat = get_request_data(js_tag="ssub.json",url="https://figshare.com/ndownloader/files/40084921")
+        elif db == 'supercon_chem':
+            dat = get_request_data(js_tag="supercon_chem.json",url="https://figshare.com/ndownloader/files/40719260")
+        elif db == 'mag2d_chem':
+            dat = get_request_data(js_tag="mag2d_chem.json",url="https://figshare.com/ndownloader/files/40720004")
+
         n_features = 145
+        col_id = 'formula'
+
     else:
         dat = data(db)
         n_features = 273
 
+    df = pd.DataFrame(dat)
+
+    # some cleaning
+    if  db == 'supercon_chem':
+        df['formula']= df['formula'].apply(lambda x: x.replace('+',''))
+        df['formula']= df['formula'].apply(lambda x: x.replace('-',''))
+        df['formula']= df['formula'].apply(lambda x: x.replace('=z',''))
+        df['formula']= df['formula'].apply(lambda x: x.replace('!1.5',''))
 
     X_file = f"X_{db}.csv"
     if not os.path.exists(X_file):
-        structure = f'structure_{db}.pkl'
-        if os.path.exists(structure):
-            df = pd.read_pickle(structure)
-        else:
-            
-            df = pd.DataFrame(dat)
-            df["structure"] = df["atoms"].progress_apply(
-                lambda x: (
-                    (Atoms.from_dict(x)).get_primitive_atoms
-                ).pymatgen_converter()
-            )
-            df.to_pickle(structure)
+        # if atomic structure, get the structure
+        if db not in special_dbs:
+            structure = f'structure_{db}.pkl'
+            if os.path.exists(structure):
+                df = pd.read_pickle(structure)
+            else:           
+                df["structure"] = df["atoms"].progress_apply(
+                    lambda x: Atoms.from_dict(x).pymatgen_converter()
+                )
+                df.to_pickle(structure)
 
         df = df.sample(frac=1, random_state=123)
-        X, failed = StructureFeaturizer(df)
+        X, failed = Featurizer(df,col_id=col_id)
         X.to_csv(X_file)
-
+    
     df = pd.read_csv(X_file)
-    features = df.columns[-n_features:]
-    if 'id' in df.columns:
-        df['id'] = df['id'].astype(str)
-        df = df.set_index('id')
-    elif 'jid' in df.columns:
-        df = df.set_index('jid')
+
+    for index_name in ['id','jid','SNUMAT_id']:
+        if index_name in df.columns:
+            df.set_index(index_name,inplace=True)
+            df.index = df.index.astype(str)
 
     for prop in get_props(db):   
-    # for prop in ['slme']:
-
+        print('')
+        print('----------------------------')
+        print('')
         print("Running", db, prop)
 
-        if task == 'SinglePropertyPrediction':
-            fname = f"AI-{task}-{prop}-{db}-test-mae.csv"
-        elif task == 'SinglePropertyClass':
-            fname = f"AI-{task}-{prop}-{db}-test-acc.csv"
+        fname = f"AI-{task}-{prop}-{db}-test-mae.csv"
 
         # skip this loop if the file already exists
         if os.path.exists(fname) or os.path.exists(fname + ".zip"):
@@ -239,32 +253,29 @@ for db in ['dft_3d' ]: #'hmof','qm9','megnet','qe_tb', 'dft_3d', 'ssub',
         n_val = len(val)
         n_test = len(test)
 
-        print("number of training samples", len(train))
-        print("number of validation samples", len(val))
-        print("number of test samples", len(test))
+        print("number of training samples", n_train)
+        print("number of validation samples", n_val)
+        print("number of test samples", n_test)
 
-        ids = list(train.keys()) + list(val.keys()) + list(test.keys())
-        y = list(train.values()) + list(val.values()) + list(test.values())
-        X = df.loc[ids,features]
-
-        X = np.array(X)
-        y = np.array(y).reshape(-1, 1).astype(np.float64)
+        ids = list(train.keys()) + list(val.keys()) + list(test.keys())   
         id_test = ids[-n_test:]
 
-        X_train = X[:n_train]
-        y_train = y[:n_train]
+        features = df.columns[-n_features:]
+        X = df.loc[ids,features]
+        y = list(train.values()) + list(val.values()) + list(test.values())
+        X = np.array(X)
+        y = np.array(y).reshape(-1, 1).astype(np.float64)        
 
-        X_val = X[-(n_val + n_test) : -n_test]
-        y_val = y[-(n_val + n_test) : -n_test]
-
-        X_test = X[-n_test:]
-        y_test = y[-n_test:]
+        X_train, y_train = X[:n_train], y[:n_train]
+        X_val, y_val = X[-(n_val + n_test) : -n_test], y[-(n_val + n_test) : -n_test]
+        X_test, y_test = X[-n_test:], y[-n_test:]
 
         t1 = time.time()
-        reg.fit(X_train, y_train)
-        
+        reg.fit(X_train, y_train)        
         pred = reg.predict(X_test)
-
+        t2 = time.time()
+        
+        # write the predictions to a csv file
         f = open(fname, "w")
         line = "id,prediction\n"
         f.write(line)
@@ -272,14 +283,13 @@ for db in ['dft_3d' ]: #'hmof','qm9','megnet','qe_tb', 'dft_3d', 'ssub',
             line = str(j) + "," + str(k) + "\n"
             f.write(line)
         f.close()
-        t2 = time.time()
-        print("Time", t2 - t1)
-        cmd = "zip " + fname + ".zip " + fname
-        os.system(cmd)
-        
-        # remove fname
+        # zip the csv file
+        os.system("zip " + fname + ".zip " + fname)        
+        # remove the csv file
         os.remove(fname)
 
+        # print time and metrics
+        print("Time", t2 - t1)
         if task == 'SinglePropertyPrediction':
             reg_sc = regr_scores(y_test, pred)
             print(prop, reg_sc["mae"])
@@ -289,4 +299,4 @@ for db in ['dft_3d' ]: #'hmof','qm9','megnet','qe_tb', 'dft_3d', 'ssub',
             print(prop, acc)
 
 
-# %%
+
