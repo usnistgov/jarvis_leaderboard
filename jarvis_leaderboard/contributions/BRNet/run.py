832b5cf01d32bf79582330728d0e115b55ed5143
GuptaVishu2002 <32040675+GuptaVishu2002@users.noreply.github.com>
2023-05-23 14:30:46 -0500

    Add files via upload

--- jarvis_leaderboard/contributions/BRNet/run.py
+++ jarvis_leaderboard/contributions/BRNet/run.py
@@ -232,7 +232,7 @@ import os
 f=open('AI-SinglePropertyPrediction-formula_energy-ssub-test-mae.csv','w')
 f.write('id,target,prediction\n')
 for i,j,k in zip(test_ids,y_test,pred):
-    line=str(i)+','+str(j)+','+str(k)+'\n'
+    line=str(i)+','+str(j)+','+str(k[0])+'\n'
     f.write(line)
 f.close()
 cmd = 'zip AI-SinglePropertyPrediction-formula_energy-ssub-test-mae.csv.zip AI-SinglePropertyPrediction-formula_energy-ssub-test-mae.csv'
