512cd4aee8a438112096b07b852251d712fe95ce
kruskallin <kruskallin@tamu.edu>
2023-06-02 02:38:56 -0500

    Adding contribution by kruskallin_potnet.

---
+++ jarvis_leaderboard/contributions/potnet/run.sh
@@ -0,0 +1,2 @@
+jarvis_populate_data.py --benchmark_file AI-SinglePropertyPrediction-formation_energy_peratom-dft_3d --output_path=Out
+python main.py --config configs/potnet.yaml --output_dir output --data_root Out/ --checkpoint checkpoints/formation_energy_peratom/checkpoint.pt --testing
