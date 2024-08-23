98326c3babede8d499d536fc6aa724dcda1d42ed
knc6 <writetokamal.1989@gmail.com>
2024-08-23 18:11:38 -0400

    Add AI scripts.

---
+++ jarvis_leaderboard/installations/AI/atomgpt_linux_colab.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install -q atomgpt bitsandbytes
+
+# Example: https://colab.research.google.com/github/knc6/jarvis-tools-notebooks/blob/master/jarvis-tools-notebooks/atomgpt_example.ipynb
