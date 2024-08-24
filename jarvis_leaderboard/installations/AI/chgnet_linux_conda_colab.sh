058801e552876d216c36621b8c17387c53f45ee5
knc6 <writetokamal.1989@gmail.com>
2024-08-23 20:47:18 -0400

    Rename files.

---
+++ jarvis_leaderboard/installations/AI/chgnet_linux_conda_colab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+conda install chgnet pytorch torchvision torchaudio pytorch-cuda -c pytorch -c nvidia --quiet -y
+
+# Example: https://colab.research.google.com/github/knc6/jarvis-tools-notebooks/blob/master/jarvis-tools-notebooks/Train_CHGNet.ipynb 
+
---
+++ jarvis_leaderboard/installations/AI/matgl_linux_conda_colab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+conda install matgl dgl=2.1.0 pytorch torchvision torchaudio pytorch-cuda -c pytorch -c nvidia --quiet -y
+
+# Example: https://colab.research.google.com/github/knc6/jarvis-tools-notebooks/blob/master/jarvis-tools-notebooks/Matgl_mlearn.ipynb 
+
