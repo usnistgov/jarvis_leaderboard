8ff16b1c7398fabd8b9961aa993c4024fdf22293
knc6 <writetokamal.1989@gmail.com>
2024-10-12 14:26:17 -0400

    QE fix.

--- jarvis_leaderboard/installations/ES/qe_linux_make_colab.sh
+++ jarvis_leaderboard/installations/ES/qe_linux_make_colab.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+sudo apt-get update
 sudo apt-get install -y libfftw3-3 libfftw3-dev libfftw3-doc
 git clone https://github.com/QEF/q-e.git
 cd q-e
-!DFLAGS='-D__FFTW3 ' FFT_LIBS='-lfftw3'  ./configure
+DFLAGS='-D__FFTW3 ' FFT_LIBS='-lfftw3'  ./configure
 make pw
 # make ph
 # Example: https://colab.research.google.com/github/knc6/jarvis-tools-notebooks/blob/master/jarvis-tools-notebooks/JARVIS_QuantumEspressoColab_Basic_Example.ipynb
