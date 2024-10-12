dff028388d227e0f8b682cfee1dcef96d5ec88e7
knc6 <writetokamal.1989@gmail.com>
2024-10-12 14:11:29 -0400

    GPAW fix.

--- jarvis_leaderboard/installations/ES/gpaw_linux_pip_colab.sh
+++ jarvis_leaderboard/installations/ES/gpaw_linux_pip_colab.sh
@@ -4,6 +4,6 @@ sudo apt-get install libxc-dev
 sudo apt install python3-mpi4py cython3 libxc-dev gpaw-data
 pip -q install ase 
 pip -q install gpaw
-gpaw test
+###gpaw test
 
 #Example: https://colab.research.google.com/github/knc6/jarvis-tools-notebooks/blob/master/jarvis-tools-notebooks/GPAW_Colab.ipynb
