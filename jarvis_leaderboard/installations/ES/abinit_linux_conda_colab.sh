38c1328895e0189a8276191f197b19a9bb1cc4d8
knc6 <writetokamal.1989@gmail.com>
2024-10-12 13:51:37 -0400

    conda-forge

--- jarvis_leaderboard/installations/ES/abinit_linux_conda_colab.sh
+++ jarvis_leaderboard/installations/ES/abinit_linux_conda_colab.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-conda install pandas numpy matplotlib abinit abipy --quiet
+conda install pandas numpy matplotlib abinit abipy -c conda-forge --quiet
 
 # Example: https://colab.research.google.com/github/knc6/jarvis-tools-notebooks/blob/master/jarvis-tools-notebooks/ABINIT_Example.ipynb
